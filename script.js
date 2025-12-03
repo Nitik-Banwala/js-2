80defac3082642e60c9e555162551c65309bebfa
NITIK <nitik@NITIKs-MacBook-Air.local>
2025-12-03 12:16:30 +0530

    Initial commit of my project

---
+++ script.js
@@ -0,0 +1,9 @@
+function openSidebar() {
+    document.getElementById("sidebar").classList.remove("-translate-x-full");
+}
+function closeSidebar() {
+    document.getElementById("sidebar").classList.add("-translate-x-full");
+}
+function toggleSidebar() {
+    document.getElementById("sidebar").classList.toggle("-translate-x-full");
+}
